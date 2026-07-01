34bc3b00d3c691c938107dd158195f1527fc167d
SIGMAVALEN <dragon911yt@gmail.com>
2026-07-01 11:52:13 -0300

    commit

--- src/config.js
+++ src/config.js
@@ -1,14 +1,13 @@
 
 
 export const config = {
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
 
   dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
+  dbPort: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 6543,
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
 
   jwtSecret: process.env.JWT_SECRET,
 };
- 
